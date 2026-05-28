22ef3f1fee40cbdc3aa12cd98e005dcbdd3e0eab
ArvindIyer1 <arvind.cooliyer@gmail.com>
2026-05-29 00:17:21 +0530

    Completed Chat-Application

---
+++ backend/src/server.ts
@@ -0,0 +1,41 @@
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import express from 'express';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: { origin: '*' }
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('joinRoom', async (data: { userName: string; room: string }) => {
+        if (socket.rooms.has(data.room)) return;
+        await socket.join(data.room);
+        socket.to(data.room).emit('roomNotice', data.userName);
+    });
+
+    socket.on('chatMessage', (data: { msg: any; room: string }) => {
+        socket.to(data.room).emit('chatMessage', data.msg);
+    });
+
+    socket.on('typing', (data: { userName: string; room: string }) => {
+        socket.to(data.room).emit('typing', data.userName);
+    });
+
+    socket.on('stopTyping', (data: { userName: string; room: string }) => {
+        socket.to(data.room).emit('stopTyping', data.userName);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+app.get('/', (req, res) => { res.send('ok'); });
+
+server.listen(3000, () => {
+    console.log('server running on port 3000');
+});
